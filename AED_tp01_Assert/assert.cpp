c5e5b18638be5ac744d0741f604d930f1c0e626d
gsaidschicchi <166030111+gsaidschicchi@users.noreply.github.com>
2024-05-15 11:41:51 -0300

    Create assert.cpp

---
+++ AED_tp01_Assert/assert.cpp
@@ -0,0 +1,16 @@
+/**************************
+Ejemplos Assert
+***************************/
+
+#include <cassert>
+
+int main() {
+
+    //int
+    assert(2+2 == 4);
+    
+    //bool
+    assert(false + true == 1);
+    
+    return 0;
+}
